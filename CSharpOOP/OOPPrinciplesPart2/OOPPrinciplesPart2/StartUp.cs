665942b2955a8ebb0b007e38f427ab35996ada6d
GeorgiLalov <lalov.georgi.l@gmail.com>
2016-06-27 20:48:19 +0300

    Add Very Base Structure, not even seen the lecture

---
+++ CSharpOOP/OOPPrinciplesPart2/OOPPrinciplesPart2/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPPrinciplesPart2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
