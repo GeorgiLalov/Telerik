0135c419154905cb6eb04c508b95c51f041457dd
GeorgiLalov <lalov.georgi.l@gmail.com>
2016-07-03 17:21:41 +0300

    Change exception, not finished

--- CSharpOOP/OOPPrinciplesPart2/RangeExceptions/InvalidRangeException.cs
+++ CSharpOOP/OOPPrinciplesPart2/RangeExceptions/InvalidRangeException.cs
@@ -2,7 +2,7 @@
 
 namespace RangeExceptions
 {
-    public class InvalidRangeException<T> : Exception
+    public class InvalidRangeException<T> : ApplicationException
     {
         public InvalidRangeException(string message, T start, T end) : base(message)
         {
